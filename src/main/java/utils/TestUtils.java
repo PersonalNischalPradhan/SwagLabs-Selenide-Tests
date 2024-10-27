6867b0d9b9628938ccd104cc46346163c7dcbd7c
Nischal <nischal.pradhan20@gmail.com>
2024-10-27 16:20:27 +0000

    [SWAG-007] test utils updated to run script in git hub

--- src/main/java/utils/TestUtils.java
+++ src/main/java/utils/TestUtils.java
@@ -1,18 +1,19 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class TestUtils {
 
-
     public static String getProperty(String key) {
-
         Properties config = new Properties();
-        String user_dir=(System.getProperty("user.dir"));
-        try (FileInputStream fileInputStream = new FileInputStream(user_dir+"\\src\\test\\resources\\SwagLabsTestData.properties")) {
-            config.load(fileInputStream);
+
+        try (InputStream inputStream = TestUtils.class.getClassLoader().getResourceAsStream("SwagLabsTestData.properties")) {
+            if (inputStream == null) {
+                throw new IOException("Property file 'SwagLabsTestData.properties' not found in the classpath");
+            }
+            config.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
